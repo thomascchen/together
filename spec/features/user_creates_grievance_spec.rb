aeebd523e0ae7f2c56e1f52c0685ebb86108f61d
Thomas Chen <chen.thomas.c@gmail.com>
2015-07-04 16:35:21 -0400

    Clean up Hound issues

--- spec/features/user_creates_grievance_spec.rb
+++ spec/features/user_creates_grievance_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
-feature 'user creates in', %Q{
+feature 'user creates in', %{
   As an authorized user,
   I want to add a grievance,
   So that my neighbors can help me solve it
 } do
 
-# Acceptance Criteria
-# [ ] User is able to submit a new grievance
-# [ ] After successful submission, user is redirected to a list of grievances
-# [ ] User
+  # Acceptance Criteria
+  # [ ] User is able to submit a new grievance
+  # [ ] After successful submission, user is redirected to a list of grievances
+  # [ ] User
 
   scenario 'specify valid credentials' do
 
