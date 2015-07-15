4e8ad35ca6e430c534451e692aab73aab0b24fc4
Thomas Chen <chen.thomas.c@gmail.com>
2015-07-14 20:08:38 -0400

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -20,8 +20,6 @@ Rails.application.routes.draw do
     resources :problems, only: [:index]
   end
 
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
