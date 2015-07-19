2b2c10201770af795560b4623fe1d1c03679c24e
Thomas Chen <chen.thomas.c@gmail.com>
2015-07-19 10:18:27 -0400

    Add additional scenarios for 'user upvotes problem'

--- spec/features/user_upvotes_problem_spec.rb
+++ spec/features/user_upvotes_problem_spec.rb
@@ -12,6 +12,9 @@ feature 'user upvotes problem', %{
   # [x] User cannot upvote on their own problem
   # [x] User can only upvote once per problem
   # [x] User must be authenticated to vote
+  # [x] User cannot upvote solved problems
+  # [x] Score is displayed on both index and show page for unsolved problems,
+  #     but not on solved problem show page
 
   let(:category) { Category.create(name: "Heat and Essential") }
   let(:urgency_level) { UrgencyLevel.create(id: 1, name: "Immediate") }
@@ -30,16 +33,26 @@ feature 'user upvotes problem', %{
 
   scenario 'authenticated user upvotes and cancels vote on problem', js: true do
     sign_in(FactoryGirl.create(:user))
-
     click_on "Upvote"
-
     visit root_path
+
     expect(page).to have_content('Score: 1')
     expect(page).to_not have_content('Upvote')
 
     click_on 'Cancel vote'
-
     visit root_path
+
+    expect(page).to have_content('Score: 0')
+    expect(page).to_not have_link('Cancel vote')
+
+    click_on problem.title
+    click_on 'Upvote'
+
+    expect(page).to have_content('Score: 1')
+    expect(page).to_not have_content('Upvote')
+
+    visit problem_path(problem)
+    click_on 'Cancel vote'
 
     expect(page).to have_content('Score: 0')
     expect(page).to_not have_link('Cancel vote')
@@ -53,6 +66,25 @@ feature 'user upvotes problem', %{
 
   scenario 'unauthenticated user cannot vote on problem' do
     visit root_path
+
+    expect(page).to_not have_link('Upvote')
+  end
+
+  scenario 'authenticated user cannot vote on solved problems' do
+    solved_problem = FactoryGirl.create(
+      :problem,
+      user: user,
+      category: category,
+      urgency_level: urgency_level,
+      status: solved_status
+    )
+
+    sign_in(FactoryGirl.create(:user))
+    click_on 'Solved Problems'
+
+    expect(page).to_not have_link('Upvote')
+
+    click_on solved_problem.title
 
     expect(page).to_not have_link('Upvote')
   end
